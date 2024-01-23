13ef4ea335722d496866e54b3fec4b2f8c2f9074
asror <a.aliqulov@student.inha.uz>
2024-01-23 15:39:21 +0500

    Initial Commit

---
+++ Helper/HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelperApp.swift
+//  Helper
+//
+//  Created by Asror Aliqulov on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ef4c4fab4149e9224eb726e79717093943de9c21
asror <a.aliqulov@student.inha.uz>
2024-01-24 16:28:10 +0500

    data model change

--- Helper/Data.swift
+++ Helper/Data.swift
@@ -7,81 +7,94 @@
 
 import Foundation
 
+//MARK: - structs
+
 struct Profile: Codable {
-    var profilePhoto: Data
+    var id: String
+    
+    var profilePhoto: Data?
     var fullName: String
     var location: String
     var phoneNumber: String
     var role: Role
 }
 
-enum Role: Codable {
-    case employer(
-        rating: Double,
-        posts: [Post],
-        quickHelpers: [QuickHelp]
-    )
-    case employee(
-        rating: Double,
-        abilities: [String],
-        completedJobs: [Post],
-        completedQuickHelps: [QuickHelp],
-        currentStatus: EmployeeStatus
-    )
+struct Post: Codable {
+    var id: String
+    var creatorId: String
+    
+    var postPrivacyLevel: PostPrivacyLevel
+    var jobName: String
+    var jobDescription: String
+    var tags: [String]?
+    var startingDate: Date?
+    var approximateDuration: Int?
+    var numberOfPeopleNeeded: Int?
+    var relevantImages: [Data]?
+    var postStatus: PostStatus
 }
 
-enum EmployeeStatus: Codable {
-    case working(currentWork: Post)
-    case busy
-    case free
+struct EmployeeResponse: Codable {
+    var id: String
+    var postId: String
+    
+    var wage: Double
+    var metric: String
+    var reasoning: String?
+}
+
+struct QuickHelp: Codable {
+    var id: String
+    var creatorId: String
+    
+    var jobName: String
+    var jobDesription: String
+    var numberOfPeopleNeeded: Int
+    var hourlyWage: Double
+    var review: [Review]?
 }
 
 struct Review: Codable {
+    var id: String
+    var creatorId: String
+    
     var stars: Int
     var comment: String
     var relevantImages: [Data]?
 }
 
-struct QuickHelp: Codable {
-    var jobName: String
-    var jobDesription: String
-    var numberOfPeopleNeeded: Int
-    var hourlyWage: Double
-    var review: Review?
+//MARK: - enums
+
+enum Role: Codable {
+    case employer(
+        rating: Double?,
+        posts: [Post]?,
+        quickHelpers: [QuickHelp]?
+    )
+    case employee(
+        currentStatus: EmployeeStatus,
+        rating: Double?,
+        abilities: [String]?,
+        completedJobs: [Post]?,
+        completedQuickHelps: [QuickHelp]?
+    )
 }
 
-struct Post: Codable {
-    var postPrivacyLevel: PostPrivacyLevel
-    var jobName: String
-    var jobDescription: String
-    var tags: [String]?
-    var startingDate: Date
-    var approximateDuration: ApproximateDuration
-    var numberOfPeopleNeeded: NeededPeople
-    var relevantImages: [Data]?
-    var review: Review?
-    var postStatus: PostStatus
-}
-
-enum PostStatus: String, Codable {
-    case notStarted
-    case ongoing
-    case finished
+enum PostStatus: Codable {
+    case searching(appliedPeople: [Profile]?, acceptedPeople: [Profile]?)
+    case ongoing(workingPeople: [Profile]?)
+    case finished(workedPeople: [Profile]?, reviews: [Review]?)
 }
 
 enum PostPrivacyLevel: String, Codable {
     case `private`
     case `public`
 }
-enum ApproximateDuration: Codable {
-    case approximate(days: Int)
-    case undefined
+enum EmployeeStatus: Codable {
+    case working(currentWork: Post)
+    case busy
+    case free
 }
-enum NeededPeople: Codable {
-    case exact(numberOfPeople: Int)
-    case undefined
-}
-
 
 
 
--- Helper/HomeView.swift
+++ Helper/HomeView.swift
@@ -16,15 +16,15 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            Text("Home View")
-            ScrollView {
-                VStack {
-                    ForEach(0..<19) { _ in
-                        PostCardView()
+            List {
+                ForEach(0..<19) { number in
+                    HStack {
+                        Circle()
+                            .frame(width: 60)
+                        Text("\(number)")
                     }
                 }
             }
-            .padding(.horizontal)
             .navigationTitle("Helper")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
@@ -41,63 +41,8 @@ struct HomeView: View {
         
 }
 
-struct PostCardView: View {
-    @State private var postCardState: CardState = .compact
-    var body: some View {
-        VStack {
-            switch postCardState {
-            case .compact:
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Title")
-                                .font(.title2)
-                            Label("Tashkent, Chilanzar", systemImage: "location")
-                                .foregroundStyle(.gray)
-                            
-                        }
-                        Spacer()
-                        Circle()
-                            .frame(width: 60, height: 60)
-                    }
-                }
-                .padding()
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .onTapGesture {
-                    withAnimation {
-                        postCardState = .expanded
-                    }
-                }
-            case .expanded:
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Title")
-                                .font(.title2)
-                            Label("Tashkent, Chilanzar", systemImage: "location")
-                                .foregroundStyle(.gray)
-                            
-                        }
-                        Spacer()
-                        Circle()
-                            .frame(width: 60, height: 60)
-                    }
-                }
-                .frame(height: 200)
-                .padding()
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .onTapGesture {
-                    withAnimation {
-                        postCardState = .compact
-                    }
-                }
-            }
-        }
-    }
-}
+
     
-    #Preview {
-        HomeView()
-    }
+#Preview {
+    HomeView()
+}

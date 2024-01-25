916a03c588cd8bf1675f319a9c2752f395006ee9
Davron Abdukhakimov <davron0506150@gmail.com>
2024-01-25 16:14:12 +0500

    welcome screen added

---
+++ Helper/Screens/WelcomeView.swift
@@ -0,0 +1,68 @@
+//
+//  WelcomeView.swift
+//  Helper
+//
+//  Created by Davron Abdukhakimov on 25/01/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @State var pageOrder = 0
+    let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    var body: some View {
+        NavigationStack{
+            VStack{
+                TabView(selection: $pageOrder){
+                    Text("Tab Content 1")
+                        .tabItem { Text("Tab Label 1") }
+                        .tag(0)
+                    Text("Tab Content 2")
+                        .tabItem { Text("Tab Label 2") }
+                        .tag(1)
+                    Text("Tab Content 3")
+                        .tabItem { Text("Tab Label 3") }
+                        .tag(2)
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .onAppear() {
+                    UIPageControl.appearance().currentPageIndicatorTintColor = .blue
+                    UIPageControl.appearance().pageIndicatorTintColor = .gray
+                }
+                HStack{
+                    Button{
+                        
+                    }label: {
+                        Text("Sign Up")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                    .buttonStyle(.bordered)
+                    .padding(.trailing,50)
+                    Button{
+                        
+                    }label: {
+                        Text("Log In")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+            .navigationTitle("Helper")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .onReceive(timer){ _ in
+            withAnimation {
+                pageOrder += 1
+                if pageOrder == 3{
+                    pageOrder = 0
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}

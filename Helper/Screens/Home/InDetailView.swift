fb8e02363e591c0163a3ecdff247b4a7132f7a66
asror <a.aliqulov@student.inha.uz>
2024-01-25 16:07:55 +0500

    inDetail View added

---
+++ Helper/Screens/Home/InDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  InDetailView.swift
+//  Helper
+//
+//  Created by Asror Aliqulov on 25/01/24.
+//
+
+import SwiftUI
+
+struct InDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InDetailView()
+}

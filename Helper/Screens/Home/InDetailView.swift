161e6eb7113b49ca0cdacc78ef7d013c5b7d7870
asror <a.aliqulov@student.inha.uz>
2024-01-25 16:08:20 +0500

    some changes

--- Helper/Screens/Home/InDetailView.swift
+++ Helper/Screens/Home/InDetailView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct InDetailView: View {
+    var post = mockPost
+    @State var response = mockResponse
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("Creator") {
+                    NavigationLink {
+                        ProfileView()
+                    } label: {
+                        Label("Asror Aliqulov", systemImage: "person.circle")
+                    }
+                }
+                Section("Job Information") {
+                    Text(post.jobName)
+                    Text(post.jobDescription)
+                }
+                Section("Accepted People") {
+                    HStack {
+                        Text("Spots left:")
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Text("3")
+                            .fontWeight(.semibold)
+                    }
+                    ForEach(0..<5, id: \.self) { _ in
+                        NavigationLink {
+                            ProfileView()
+                        } label: {
+                            Label("Asror Aliqulov", systemImage: "person")
+                        }
+                    }
+                    
+                }
+                
+                Section("Enter Your Offer") {
+                    TextField("Price", text: $response.wage)
+                    TextField("Enter your metric such as square meter or for whole job", text: $response.metric)
+                    TextField("Explain why!", text: $response.reasoning, axis: .vertical)
+                }
+                Button {
+                    print(response)
+                } label: {
+                    Text("Apply")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                }
+                .disabled(response.wage.isEmpty || response.metric.isEmpty)
+                .buttonStyle(.borderedProminent)
+                .listRowBackground(Color.clear)
+            }
+        }
     }
 }
 
